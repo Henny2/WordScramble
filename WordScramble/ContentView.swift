0b54642a8350de1f11d2ff5984d9e414395d873f
Henny2 <henrieke.baunack@web.de>
2023-10-23 20:39:53 -0700

    first code snippets

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Put in a word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self){ word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
-        .padding()
+    }
+    
+    func addNewWord() -> Void {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+       
+        // more validation to come
+        withAnimation{
+            usedWords.insert(newWord, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame(){
+        
     }
 }
 
